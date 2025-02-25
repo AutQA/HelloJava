508dde6f62e86c66894c200e8d257417963a6606
vivek <vivek@WS-092>
2025-02-25 15:18:30 +0530

    test edit changes

--- src/beginner/HelloJava.java
+++ src/beginner/HelloJava.java
@@ -5,7 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) 
 	{
 		System.out.println("HelloJava");
-		System.out.println("HelloJavaEdit");
+		System.out.println("HelloSelenium");
 
 	}
 
